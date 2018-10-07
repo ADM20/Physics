88b5d6132b09cc44df7f6b85c7beb55f7979bfd0
ADM20 <40206116@live.napier.ac.uk>
2018-10-07 16:13:41 +0100

    Almost works
    Damper is broke

--- main.cpp
+++ main.cpp
@@ -68,8 +68,8 @@ int main()
 		particles[i].setMesh(Mesh("resources/models/sphere.obj"));
 		particles[i].scale(glm::vec3(.1f, .1f, .1f));
 		particles[i].getMesh().setShader(Shader("resources/shaders/solid.vert", "resources/shaders/solid_blue.frag"));
-		particles[i].setPos(glm::vec3(-4, 9.9f,0.0f ));
-		particles[i].setVel(glm::vec3(10, 0.0f, 0.0f));
+		particles[i].setPos(glm::vec3(1.0f, 9.9f,0.0f ));
+		particles[i].setVel(glm::vec3(0.0f, 0.0f, 0.0f));
 		//particles[i].translate(glm::vec3(i, 0.0f, 0.0f));
 	}
 
@@ -97,7 +97,23 @@ int main()
 	//bounding Box
 	glm::vec3 bBox = glm::vec3(5.0f, 10.0f, 5.0f);
 	//friction damper
-	float damper = 1.0f;
+	float damper = 0.9f;
+	
+	//Cone
+	glm::vec3 topCone = glm::vec3(0.0f, 4.0f, 0.0f);
+	glm::vec3 bottomCone = glm::vec3(0.0f);
+	//top and bottom radius
+	float topConeR = 2.0f;
+	float bottomConeR = 3.0f;
+	//Cone total hieght if it went to a point
+	float heightCone = (topConeR*(topCone.y - bottomCone.y)) / (topConeR - bottomConeR);
+	//Where the cone would be a point if it got that far
+	glm::vec3 tip = glm::vec3(topCone);
+	tip.y -= heightCone;
+	//force
+	glm::vec3 forceCone = glm::vec3(0.0f);
+	//force coefficient
+	float coneForceCo = 20.0f;
 	
 	
 	// Game loop
@@ -116,21 +132,76 @@ int main()
 
 		while (accumulator >= dt)
 		{
+			
 			for (int i = 0; i < numberOfParticles; i++)
 			{
 				/*
 				**	SIMULATION
 				*/
-
+				glm::vec3 v = particles[i].getVel();
+				glm::vec3 r = particles[i].getPos();
 				//compute forces
 				particles[i].setAcc(g);
 
 
-				particles[i].setVel(particles[i].getVel() + particles[i].getAcc() * dt);
-				particles[i].translate(particles[i].getVel() * dt);
+				//particles[i].setVel(particles[i].getVel() + particles[i].getAcc() * dt);
+				//particles[i].translate(particles[i].getVel() * dt);
+
+				//crosses with cone
+				//height
+				if (r.y < topCone.y && r.y >= bottomCone.y)
+				{
+					//cone at the hieght of the particle
+					glm::vec3 heightConeNow = glm::vec3(topCone);
+					heightConeNow.y = r.y;
+					//radius of cone at height
+					float coneR = bottomConeR + (heightConeNow.y / (topCone.y - bottomCone.y))*(topConeR - bottomConeR);
+					//distance from center to particle
+					float newR = glm::length(heightConeNow - r);
+					//radius
+					if (newR < coneR)
+					{
+						//direction of force
+						glm::vec3 fdir = r - tip;
+						//radius to point at top of cone
+						float topR = heightCone * (newR / ((heightCone - (topCone.y - bottomCone.y)) + heightConeNow.y));
+						//point at top at new top radius
+						glm::vec3 projection = r - topCone;
+						projection.y = 0.0f;
+						if (projection != glm::vec3(0.0f)) {
+							projection = glm::normalize(projection);
+						}
+						glm::vec3 topPoint = projection * topR;
+						topPoint += topCone;
+						//vector from point to top of cone through particle
+						glm::vec3 dirTop = glm::vec3(topPoint - tip);
+						forceCone = ((dirTop - fdir) * (coneR - newR))*coneForceCo;
+					}
+					else
+					{
+						forceCone *= 0;
+					}
+				}
+				else
+				{
+					forceCone *= 0;
+				}
+
+				//calculate drag
+				//glm::vec3 drag = 0.5 * 1.225 * -v * glm::length(v) * 1.05 * 0.01;
+				//total force
+				glm::vec3 F =  g + forceCone;
+				//a = F/m
+				particles[i].setAcc((F) / particles[i].getMass());
+				//semi implicit Eular
+				v += dt * particles[i].getAcc();
+				r += dt * v;
+				//set postition and velocity
+				particles[i].setPos(r);
+				particles[i].setVel(v);
+
 
 				//contact with bounding box
-
 				for (int j = 0; j < 3; j++)
 				{
 					if (particles[i].getPos().y <= 0.0f)
@@ -152,11 +223,11 @@ int main()
 				
 				//if particle goes over the blow dryers area of influence
 
-				if (0 < particles[i].getPos().x && particles[i].getPos().x < 2 && 0 < particles[i].getPos().y && particles[i].getPos().y < 4 && -2 < particles[i].getPos().z &&  particles[i].getPos().z < 2)
-				{
-					//std::cout << particles[i].getPos().x << std::endl;
-					particles[i].getVel()[1] *= -1.0f;
-				}
+				//if (0 < particles[i].getPos().x && particles[i].getPos().x < 2 && 0 < particles[i].getPos().y && particles[i].getPos().y < 4 && -2 < particles[i].getPos().z &&  particles[i].getPos().z < 2)
+				//{
+				//	//std::cout << particles[i].getPos().x << std::endl;
+				//	particles[i].getVel()[1] *= -1.0f;
+				//}
 
 				accumulator -= dt;
 				t += dt;
