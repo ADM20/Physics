b0c36fa13a33c6c899e4e26e280c3359059ca121
ADM20 <40206116@live.napier.ac.uk>
2018-11-30 09:23:05 +0000

    Revert "no"
    This reverts commit 430335f704b005237d47ff79ceb724482d7b645f.

--- main.cpp
+++ main.cpp
@@ -96,23 +96,23 @@ int main()
 
 	Mesh sphereMesh = Mesh::Mesh("resources/models/sphere.obj");
 
-	////rigid body
-	//RigidBody rb = RigidBody::RigidBody();
-	//Mesh m = Mesh::Mesh(Mesh::MeshType::CUBE);
-	//rb.setMesh(m);
-	//rb.getMesh().setShader(poolTable);
-	//rb.scale(glm::vec3(1.0f, 3.0f, 1.0f));
-	//rb.translate(glm::vec3(0.0f, 5.0f, 0.0f));
-	//rb.setMass(2.0f);
-	//rb.setVel(glm::vec3(5.0f, 0.0f, 0.0f));
-	////rb.setAngVel(glm::vec3(0.0f, 0.0f, 0.5f));
-	//rb.setAngVel(glm::vec3(0.0f, 1.0f, 0.2f));
+	//rigid body
+	RigidBody rb = RigidBody::RigidBody();
+	Mesh m = Mesh::Mesh(Mesh::MeshType::CUBE);
+	rb.setMesh(sphereMesh);
+	rb.getMesh().setShader(lambert);
+	rb.scale(glm::vec3(1.0f, 3.0f, 1.0f));
+	rb.translate(glm::vec3(0.0f, 5.0f, 0.0f));
+	rb.setMass(2.0f);
+	rb.setVel(glm::vec3(5.0f, 0.0f, 0.0f));
+	//rb.setAngVel(glm::vec3(0.0f, 0.0f, 0.5f));
+	rb.setAngVel(glm::vec3(0.0f, 1.0f, 0.2f));
 
-	////add forces - take out for testing impulses
-	//rb.addForce(gravity);
-	////coefficient of restitution
-	////rb.setCor(1.0f);
-	//rb.setCor(0.6f);
+	//add forces - take out for testing impulses
+	rb.addForce(gravity);
+	//coefficient of restitution
+	//rb.setCor(1.0f);
+	rb.setCor(0.6f);
 
 	/************************************/
 	// Game loop
@@ -131,8 +131,104 @@ int main()
 			/*
 			**	SIMULATION
 			*/
-			
-			
+			//calculate inverse inertia with rotation
+			glm::mat3 inInertia = glm::mat3(rb.getRotate()) * rb.getInertia() * glm::mat3(glm::transpose(rb.getRotate()));
+			//use this to find the lowest point after the collision
+			float lowestY = 0;
+			//loop every vertex to check for collisons
+			for (int i = 0; i < rb.getMesh().getVertices().size(); i++)
+			{
+				//cube vector
+				glm::vec4 cube = rb.getMesh().getModel() * glm::vec4(glm::vec3(rb.getMesh().getVertices()[i].getCoord()), 1.0f);
+				//if point below the plane detect the collision
+				if (plane.getPos().y >= cube.y)
+				{
+					//find the lowest point of collision
+					if (plane.getPos().y - cube.y > lowestY)
+						lowestY = plane.getPos().y - cube.y;
+					collisions.push_back(glm::vec3(cube));
+				}
+			}
+			//if there was a collision
+			if (collisions.size() > 0)
+			{
+				rb.setPos(1, rb.getPos().y + lowestY);
+				glm::vec3 collisionCentre;
+
+				for (glm::vec3 collision : collisions)
+				{
+					collision.y += lowestY;
+					//add all collisions
+					collisionCentre += collision;
+				}
+				//calculate mean
+				collisionCentre = collisionCentre / collisions.size();
+				//noramal of plane
+				glm::vec3 normal(0.0f, 1.0f, 0.0f);
+				glm::vec3 position = collisionCentre - rb.getPos();
+				//reletive velocity
+				glm::vec3 vr = (rb.getVel() + glm::cross(rb.getAngVel(), position));
+				//tangencial impulse
+				glm::vec3 vt = vr - glm::dot(vr, normal) * normal;
+
+				//top number
+				float topNum = (-1 + -rb.getCor()) * glm::dot(vr, normal);
+				//denominator
+				float denominator = pow(rb.getMass(), -1) + glm::dot(normal, glm::cross(inInertia * glm::cross(position, normal), position));
+				//impulse
+				float impulse = (topNum / denominator);
+				//apply impulse
+				applyImpulse(rb, collisionCentre, impulse, normal);
+
+				//*****FRICTION******\\
+				
+				//declare frcition impulse
+				glm::vec3 frictionimpulse;
+
+				//only use impulse if there is movement.
+				if (vt != glm::vec3(0.0f))
+				{
+					//set friction impulse
+					frictionimpulse = -0.21 * abs(impulse) * glm::normalize(vt);
+					//angular energy loss to make object stop rotating
+					rb.setAngVel(rb.getAngVel() * 0.95);
+				}
+				else
+				{
+					//set friction to 0 so that the system stops movement
+					frictionimpulse = glm::vec3(0.0f);
+				}
+				//when velocity is  too low, set it to stop moving
+				if (glm::length(rb.getVel()) < 0.2f)
+				{
+					rb.setVel(glm::vec3(0.0f));
+					rb.setAngVel(rb.getAngVel() / 1.1f);
+				}
+				if (glm::length(rb.getAngVel()) < 0.1f)
+				{
+					rb.setAngVel(glm::vec3(0.0f));
+				}
+				//friction impulse
+				applyImpulse(rb, collisionCentre, frictionimpulse);
+
+				//*****************************
+			}
+
+			//if no collision was detected, continue as normal 
+
+				//integration translation
+			rb.setAcc(rb.applyForces(rb.getPos(), rb.getVel(), t, dt));
+			rb.setVel(rb.getVel() + dt * rb.getAcc());
+			rb.translate(rb.getVel() * dt);
+
+			//intergration rotation
+			rb.setAngVel(rb.getAngVel() + dt * rb.getAngAcc());
+			glm::mat3 angVelSkew = glm::matrixCross3(rb.getAngVel());
+			glm::mat3 rotate = glm::mat3(rb.getRotate());
+			rotate += dt * angVelSkew * rotate;
+			rotate = glm::orthonormalize(rotate);
+			rb.setRotate(rotate);
+			//*******************
 			accumulator -= dt;
 			t += dt;
 		}
@@ -146,9 +242,7 @@ int main()
 		// draw groud plane
 		app.draw(plane);
 		// draw Rigid Body
-		//app.draw(rb.getMesh());
-		//draw sphere
-		//app.draw(sp.getMesh());
+		app.draw(rb.getMesh());
 		// draw objects
 		//app.draw(cube);
 		app.display();
